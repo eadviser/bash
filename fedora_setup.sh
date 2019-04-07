f6801c8e2dceae436dc3f7bca5ebf50373d0e108
jin <jin@localhost.localdomain>
2019-04-07 15:15:05 +0200

    Initial...

---
+++ fedora_setup.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+gnome_settings_sctions() {
+	gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Terminal'"
+	gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'Launch1'"
+	gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'gnome-terminal'"
+	gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Alt>F1']"
+	gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Alt>F2']"
+	gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Alt>F3']"
+	gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Alt>F4']"
+	gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>x']"
+	gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Alt>r']"
+
+	DEF_TERM_PROFILE=`gsettings get org.gnome.Terminal.ProfilesList default`
+	DEF_TERM_PROFILE=${DEF_TERM_PROFILE:1:-1}
+	echo $DEF_TERM_PROFILE
+	gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$DEF_TERM_PROFILE/ default-size-columns 170
+	gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$DEF_TERM_PROFILE/ default-size-rows 50
+	gsettings set org.gnome.desktop.interface menubar-accel ''
+	gsettings set org.gnome.desktop.wm.preferences audible-bell false
+	gsettings set org.gnome.desktop.wm.preferences button-layout 'menu:minimize,maximize,close'
+	#gsettings set org.gnome.shell.overrides dynamic-workspaces false
+	gsettings set org.gnome.mutter dynamic-workspaces false
+	gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
+	echo "Gnome Settings done."
+}
+
+package_actions() {
+	sudo dnf -y install fedora-workstation-repositories &&\
+	sudo dnf -y config-manager --set-enabled google-chrome &&\
+	sudo dnf -y update &&\
+	sudo dnf -y install nano git mc powerline google-chrome-stable apfs-fuse autoconf automake wget &&\
+	sudo dnf -y groupinstall "Development Tools" &&\
+	sudo dnf -y remove abrt*
+}
+
+macos_documents() {
+	ME=$( whoami )
+	sudo apfs-fuse /dev/sdc2 /mnt | true &&\
+	sudo rsync -ah --info=progress2 --exclude=".*" --exclude="createRamDisk*" --exclude="WoT*" --exclude="Microsoft*" /mnt/root/Users/joe/Documents/ /home/$ME/Dokumenty &&\
+	sudo rsync -ah --info=progress2 --exclude=".*" /mnt/root/Users/joe/Pictures/ /home/$ME/Obrazy
+	sudo rsync -ah --info=progress2 --include=Tahoma* --include=SFN* --exclude=* /mnt/root/Library/Fonts/ /mnt/root/System/Library/Fonts/ /home/$ME/.fonts
+	sudo rsync -ah --min-size=1 --exclude=".*" /mnt/root/Library/Fonts/Microsoft/ /home/$ME/.fonts
+	sudo chown -R $ME. /home/$ME/Dokumenty /home/$ME/.fonts /home/$ME/Obrazy &&\
+	sudo umount /mnt
+}
+
+usage_action() {
+cat << EOF
+usage:
+	-p --package run system wide package action
+	-g --gnome-settings run gnome settings action
+	-i --interactive You'll be asked for every step one by one
+	-m --macdoc rsync macos documents
+	-h --help help
+EOF
+}
+
+interactive_action() {
+	echo "Press any key to package actions..."
+	read -t 3 -n 1 && package_actions
+	echo "Press any key to Gnome Settings Actions..."
+	read -t 3 -n 1 && gnome_settings_actions
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --package )        package_actions
+				exit
+                                ;;
+	-g | --gnome-settings ) gnome-settings_action
+                                exit
+                                ;;
+        -m | --macdoc )    	macos_documents
+                                exit
+                                ;;
+        -i | --interactive ) 	interactive_action
+				exit
+                                ;;
+        -h | --help )           usage_action
+                                exit
+                                ;;
+        * )                     usage_action
+                                exit 1
+    esac
+    shift
+done
+
+usage_action
+
+
